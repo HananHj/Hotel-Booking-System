1460586c56ef72f3c1d06526b838e0ce52014087
Hanan Alhaj <121353573+HananHj@users.noreply.github.com>
2023-04-03 23:55:09 +0300

    Add files via upload

---
+++ hotel2/src/hotel2/Hotel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel2;
+/**
+ *
+ * @author Owner
+ */
+public interface Hotel {
+    public void showWiFi(int numberRoom );
+    public void checkout(); 
+    public void booking (Guest guestlnfo, int inDay, int outDay);
+    public void extensionOfReservation(int numberOfDays);
+    public void calculateNumberOfDays();
+}
+    
